419123330ee57ecfe28318072ec8be5430877660
chuan-hao, you <howtomakeaturn@hotmail.com>
2015-04-19 09:20:52 +0800

    Add frame static files.

--- Atayal/Modules/Frame/Main/views/index.blade.php
+++ Atayal/Modules/Frame/Main/views/index.blade.php
@@ -1,3 +1,62 @@
-<h1>歡迎使用Atayal CMS</h1>
-<p>You'll be proud of it.</p>
-<em>Atayal's spirit!</em>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/atayal/modules/frame/node_modules/semantic-ui-css/semantic.css">    
+        <style>
+            .main{
+                width: 960px;
+                margin: 20px auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="main nav">
+            <div class="ui main menu">
+                <div class="container">
+                    <div class="left menu">
+                        <a href="/" class=
+                        "launch item"><b>Atayal CMS</b></a>
+                        <a href='/about' class="item">About</a>
+                        <a href='/install' class="item">Install</a>
+                        <a href='/documentation' class="item">Documentation</a>
+                        <a href='/forum' class="item">Forum</a>
+                        <a href='/blog' class="item">Blog</a>
+                    </div>
+
+                    <div class="right menu">
+                        <a href="/info" class="item">Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      <div class="main container">
+        <h2 class="ui dividing header">Node</h2>
+
+        <h2 class="ui dividing header">Mimosa</h2>
+
+        <h2 class="ui dividing header">Bower</h2>
+      </div>
+
+      <div class="main footer">
+        <div class="ui divider"></div>
+
+        <div class="ui divided horizontal footer link list">
+            <div class="item">
+                Atayal
+            </div>
+
+            <div class="item">
+                v 1.0.0
+            </div>
+
+            <a href="https://github.com/Anachron/satisfy" class="item">Homepage</a>
+        </div>
+      </div>
+
+      <div class="ui active inverted main dimmer" style="display: none;">
+        <div class="loader">
+          Loading, please wait.
+        </div>
+      </div>
+    </body>
+</html>
